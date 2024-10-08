874bbd2db4089c38748c89c9552ea9516e7a0672
pboncoeur <pboncoeur@udm.ac.mu>
2024-10-09 00:21:20 +0400

    Initial commit with CI

--- test_main.py
+++ test_main.py
@@ -1,12 +1,17 @@
 # test_main.py
 import unittest
-from main import add
+from main import add, mult
 
 class TestAddFunction(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(10, 5), 15)
+
+    def test_add(self):
+        self.assertEqual(mult(2, 3), 6)
+        self.assertEqual(mult(-1, 1), -1)
+        self.assertEqual(mult(10, 5), 50)
 
 if __name__ == "__main__":
     unittest.main()
