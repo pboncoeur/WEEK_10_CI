53b87f6b12cddd3287fec8ce24658a14ea48637c
pboncoeur <pboncoeur@udm.ac.mu>
2024-10-09 00:07:44 +0400

    Initial commit with new assert in Test_main

--- test_main.py
+++ test_main.py
@@ -6,6 +6,7 @@ class TestAddFunction(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(10, 5), 15)
 
 if __name__ == "__main__":
     unittest.main()
